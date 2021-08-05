1a97bbdccbf67d11d13ed47408a78a886e3a3c13
Dreaner <dreaner2016@outlook.com>
2021-08-06 03:12:19 +0800

    Scrape data into movie.db and take explanatory notes.

--- DoubanSniper.py
+++ DoubanSniper.py
@@ -4,132 +4,176 @@
 # @FileName: DoubanSniper.py
 # @Software: PyCharm
 
+
 from bs4 import BeautifulSoup
-import re  # 正则表达式
-import urllib.error, urllib.request
-import xlwt
+import re
+import urllib.request,urllib.error
 import sqlite3
 
 
 def main():
     baseurl = "https://movie.douban.com/top250?start="
-    datalist = getData(baseurl)
-    savepath = ".\\豆瓣电影top250.xls"
-    # 保存数据到xls文档
-    saveData(datalist, savepath)
-    # askURL("https://movie.douban.com/top250?start=")
+    # 获取网页
+    datalist = getDate(baseurl)
+    dbpath = "movie.db"
+    # 保存数据
+    saveDataDB(datalist, dbpath)
 
 
-# 影片详情链接
+head = {
+        "User-Agent":"Mozilla / 5.0(Windows NT 10.0;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / "
+                     "85.0.4183.121Safari / 537.36 "
+}
+
+# 影片详情链接规则
 findLink = re.compile(r'<a href="(.*?)">')
-# 影片图片
+# 影片图片的链接
 findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
 # 影片片名
 findTitle = re.compile(r'<span class="title">(.*)</span>')
 # 影片评分
 findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
-# 影片评价人数
-findJudgeNum = re.compile(r'<span>(\d*)人评价</span>')
-# 影片介绍
-findIntro = re.compile(r'<span class="inq">(.*)</span>')
-# 影片信息
-findInfo = re.compile(r'<p class="">(.*?)</p>', re.S)
+# 评价人数
+findJudge = re.compile(r'<span>(\d*)人评价</span>')
+# 概况
+findInq = re.compile(r'<span class="inq">(.*)</span>')
+# 找到影片的相关内容
+findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
 
 
 # 爬取网页
-def getData(baseurl):
+def getDate(baseurl):
     datalist = []
-    for i in range(0, 10):  # 豆瓣top250一共10页 每页25个条目
-        url = baseurl + str(i * 25)
-        html = askURL(url)  # 保存获取到的网页的源码
-
-        # 解析数据
+    x = 1
+    # 调用获取页面信息的函数(10次)
+    for i in range(0,10):
+        url = baseurl + str(i*25)
+        html = askURL(url) # 保存获取到的网页源码
+        # 逐一解析数据
         soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):
-            # print(item)        # 查看电影item的全部信息
-            data = []  # 保存电影的所有信息
-            item = str(item)
-
+        for item in soup.find_all('div', class_="item"):    # 查找符合要求的字符串，形成列表,class加下划线表示属性值
+            data = []   # 保存一部电影的所有信息
+            item = str(item)    # 将item转换为字符串
             # 影片详情链接
             link = re.findall(findLink, item)[0]
+            # 追加内容到列表
             data.append(link)
-            # 影片图片
+
             imgSrc = re.findall(findImgSrc, item)[0]
             data.append(imgSrc)
-            # 影片片名
-            title = re.findall(findTitle, item)
-            if len(title) == 2:
-                chineseTitle = title[0]  # 中文片名
-                data.append(chineseTitle)
-                foreignTitle = title[1].replace("/", "")  # 外文片名
-                data.append(foreignTitle)
-            else:
-                data.append(title[0])
-                data.append(' ')
-            # 影片评分
-            rating = re.findall(findRating, item)[0]
-            data.append(rating)
-            # 影片评价人数
-            judgeNum = re.findall(findJudgeNum, item)
-            data.append(judgeNum)
-            # 影片介绍
-            intro = re.findall(findIntro, item)
-            if len(intro) != 0:
-                intro = intro[0].replace("。", "")
-                data.append(intro)
-            else:
-                data.append(' ')
-            # 影片信息
-            info = re.findall(findInfo, item)[0]
-            info = re.sub('<br(\s+)?/>(\s+)', " ", info)  # 去掉<br/>
-            info = re.sub('/', " ", info)  # 把/换成空格
-            data.append(info.strip())  # 去掉空格
 
-            datalist.append(data)  # 将一部处理过的电影信息放入datalist
-            # print(datalist)
+            titles = re.findall(findTitle, item)
+            if len(titles) == 2:
+                ctitle = titles[0]
+                data.append(ctitle)     # 添加中文名
+                otitle = titles[1].replace("/", "")     # 去掉无关符号
+                data.append(otitle)     # 添加外国名
+            else:
+                data.append(titles[0])
+                data.append(' ')    # 外国名如果没有则留空
+
+            rating = re.findall(findRating,item)[0]
+            data.append(rating)
+
+            judgeNum = re.findall(findJudge, item)[0]
+            data.append(judgeNum)
+
+            inq = re.findall(findInq, item)
+            if len(inq) != 0 :
+                inq = inq[0].replace("。", "")
+                data.append(inq)
+            else:
+                data.append(' ')
+
+            bd = re.findall(findBd,item)[0]
+            bd = re.sub('<br(\s+)?/>(\s+)?'," ", bd)    # 去掉<br/>
+            bd = re.sub('/', " ", bd)   # 去掉/
+            data.append(bd.strip())     # 去掉前后空格
+
+            datalist.append(data) # 把处理好的一部电影信息放入datalist
+            # print(link)
+
+            # 下载图片到本地
+            # root = "E://DoubanPic//"
+            # path = root + str(x) + '.jpg'
+            # try:
+            #     if not os.path.exists(root):
+            #         os.mkdir(root)
+            #     if not os.path.exists(path):
+            #         #r = requests.get(imgSrc, headers=head)
+            #         urllib.request.urlretrieve(imgSrc,path)
+            #         #with open(path, 'wb') as f:
+            #         #   f.write(r.content)
+            #         #   f.close()
+            #         print("下载第%d部电影封面"%(x))
+            #         x += 1
+            #     else:
+            #         print("文件保存成功")
+            # except:
+            #     print("下载失败")
     return datalist
 
 
-# 得到指定一个URL的网页内容
+# 得到指定一个url的网页内容
 def askURL(url):
-    head = {
-        "User-Agent": "Mozilla/5.0(Windows NT 10.0; Win64; x64) "
-                      "AppleWebKit / 537.36(KHTML, likeGecko) "
-                      "Chrome / 92.0.4515.107 "
-                      "Safari / 537.36 "
-                      "Edg / 92.0.902.62"
-    }
     request = urllib.request.Request(url, headers=head)
     html = ""
     try:
         response = urllib.request.urlopen(request)
         html = response.read().decode("utf-8")
-        # print(html)
     except urllib.error.URLError as e:
         if hasattr(e, "code"):
-            print(e.code)
+            print(e.code)       # 打印错误信息
         if hasattr(e, "reason"):
-            print(e.reason)
+            print(e.reason)     # 打印错误原因
     return html
 
 
-# 保存数据
-def saveData(datalist, savepath):
-    book = xlwt.Workbook(encoding="utf-8", style_compression=0)
-    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=0)
-    col = ("电影详情链接", "图片链接", "影片中文名", "影片外文名", "评分", "评价人数", "介绍", "相关信息")
-    for i in range(0, 8):
-        sheet.write(0, i, col[i])
-    for i in range(0, 250):
-        print("第%d条" % (i + 1))
-        data = datalist[i]
-        for j in range(0, 8):
-            sheet.write(i + 1, j, data[j])
+# 保存数据到database
+def saveDataDB(datalist,dbpath):
+    init_db(dbpath)
+    conn = sqlite3.connect(dbpath)
+    cur = conn.cursor()
 
-    book.save(savepath)  # 保存到xls文档
+    for data in datalist:
+        for index in range(len(data)):
+            if index == 4 or index == 5:
+                continue
+            data[index] = '"'+data[index]+'"'
+        sql = '''
+                insert into movie_top250 (info_link, pic_link, cname, ename, score, rated, instroduction, info)
+                values(%s)'''%",".join(data)
+        cur.execute(sql)
+        conn.commit()
+    cur.close()
+    conn.close()
 
 
-if __name__ == "__main__":
+# 创建数据库
+def init_db(dbpath):
+    sql = '''
+        create table movie_top250    
+        (
+        id integer primary key autoincrement,
+        info_link text,
+        pic_link text,
+        cname varchar ,
+        ename varchar ,
+        score numeric ,
+        rated numeric ,
+        instroduction text,
+        info text
+        )
+'''''
+
+    conn = sqlite3.connect(dbpath)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
     main()
-    print("爬取完毕！")
+    print("爬取完毕")
 
